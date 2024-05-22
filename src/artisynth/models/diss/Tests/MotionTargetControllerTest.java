796061f73889b317efd9174739faca8b4af4960a
AlexDenk <alexander.denk@uni-due.de>
2024-05-22 13:03:21 +0200

    Create MotionTargetControllerTest.java

---
+++ src/artisynth/models/diss/Tests/MotionTargetControllerTest.java
@@ -0,0 +1,25 @@
+package artisynth.models.diss.Tests;
+
+import maspack.util.UnitTest;
+
+public class MotionTargetControllerTest extends UnitTest {
+
+   public void testProjectToFrameSurface () {
+      // write test details here
+      
+
+   }
+   
+
+   // add other tests you want to be performed
+
+   public void test () {
+      testProjectToFrameSurface ();
+      
+   }
+
+   public static void main (String[] args) {
+      MotionTargetControllerTest tester = new MotionTargetControllerTest ();
+      tester.runtest ();
+   }
+}
